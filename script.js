fb40dd7ab9be558029e1cdeb1f68d6536e090b61
Wolfram-txt <davidmaisner@seznam.cz>
2026-02-23 11:46:31 +0100

    needs-salt update 1

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+document.getElementById("inputText").addEventListener("input", function() {
+    var text = this.value;
+    var charCount = text.length;
+    document.getElementById("charCount").textContent = "Character Count: " + charCount;
+});
